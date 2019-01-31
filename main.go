725ebdf0195185fad44e9819abeaed54987cc13a
Ryan Fleck <ryan.fleck@protonmail.com>
2019-01-31 06:15:22 -0500

    Fiddle with GOPATH

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"logging"
 	"time"
 )
 
 func main() {
 	fmt.Println("Welcome to the playground!")
-
+	logging.Log("Test")
 	fmt.Println("The time is", time.Now())
 }
